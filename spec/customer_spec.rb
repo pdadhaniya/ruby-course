4600a0c8d54510f56386fa48912bae9a33a58c54
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-29 15:11:10 -0700

    add tests to check initialize, purchase request, and complete request for customer class

---
+++ spec/customer_spec.rb
@@ -0,0 +1,45 @@
+require_relative 'spec_helper.rb'
+
+describe PuppyBreeder::Customer do
+
+
+	describe "#initialize" do
+		xit "should add a name and puppy purchased array for reach customer" do
+
+		end
+	end
+
+	describe "#purchase_request" do 
+		xit "should make a request to purchase a puppy" do
+
+		end
+
+		xit "should change the puppy's status to pending" do
+
+		end
+
+		xit "should add request to breeder's pending queue" do
+
+		end
+	end
+
+	describe "complete_purchase" do
+		xit "should close the request to purchase a puppy" do
+
+		end
+
+		xit "should change the puppy's status to sold" do
+
+		end
+
+		xit "should remove request from breeder's queue" do
+
+		end
+
+		xit "should add puppy to puppy_purchased array" do
+
+		end
+
+	end
+
+end
