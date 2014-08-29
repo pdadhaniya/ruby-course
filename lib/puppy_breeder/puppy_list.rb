85f73073c2fec72b52e5fbad8198f34a462b080d
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-29 14:28:18 -0700

    add puppy sold method to return puppy, update sold puppies list and remove from list

--- lib/puppy_breeder/puppy_list.rb
+++ lib/puppy_breeder/puppy_list.rb
@@ -3,10 +3,21 @@ module PuppyBreeder
   class PuppyList
 
   	attr_accessor :list
+  	attr_reader :sold_puppies
 
   	def initialize
   		@list = []
+  		@sold_puppies = []
   	end
 
+  	def add(puppy)
+  		@list << puppy
+  	end
+
+  	def puppy_sold(puppy)
+  		@sold_puppies << puppy
+  		@list.delete(puppy)
+  		puppy
+	end
   end
 end
