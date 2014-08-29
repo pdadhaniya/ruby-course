3e062bd94a5c3de65969c98c3b6f1ae84e3e8b42
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-29 13:31:38 -0700

    add sold method to change status to sold

--- lib/puppy_breeder/puppy.rb
+++ lib/puppy_breeder/puppy.rb
@@ -1,11 +1,19 @@
 #Refer to this class as PuppyBreeder::Puppy
 module PuppyBreeder
   class Puppy
-	attr_reader :name  	
+	attr_reader :name
+	attr_reader :status  	
 
   	def initialize(name)
   		@name = name
+  		@status = "available"
   	end
+
+  	def sold
+  		@status = "sold"
+  	end
+
+
 
   end
 end
