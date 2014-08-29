896b8069a205c62a5d02cd1db9b2d7da714d90af
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-29 11:06:43 -0700

    add self.list method to list all puppies array

--- lib/puppy_breeder/puppy.rb
+++ lib/puppy_breeder/puppy.rb
@@ -3,9 +3,18 @@ module PuppyBreeder
   class Puppy
 	attr_reader :name  	
 
+	@@all_puppies = []
+
   	def initialize(name)
   		@name = name
   	end
 
+  	def self.list
+  		@@all_puppies
+  	end
+
   end
 end
+
+
+
