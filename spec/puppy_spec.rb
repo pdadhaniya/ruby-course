9a68739bbdd232db762f29274c1bdd5894fff90b
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-29 10:54:20 -0700

    add test for initialize to check if new puppy is made with correct name

--- spec/puppy_spec.rb
+++ spec/puppy_spec.rb
@@ -1,5 +1,11 @@
 require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::Puppy do
+	describe "#initialize" do
+		it "should make a new puppy" do
+			result = PuppyBreeder::Puppy.new("Fred")
+			expect(result.name).to eq("Fred")
+		end
+	end
 
 end
