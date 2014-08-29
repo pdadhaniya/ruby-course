1443e2ab9840d273d4d6ae67d3bb546799b41ec1
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-29 15:55:08 -0700

    add purchase request method to change pending request to yes if list includes the puppy

--- lib/puppy_breeder/customer.rb
+++ lib/puppy_breeder/customer.rb
@@ -4,12 +4,19 @@ module PuppyBreeder
 
   	attr_reader :name
   	attr_reader :puppy_purchased
-  
+  	attr_reader :pending_request
+
   	def initialize(name)
   		@name = name
   		@puppy_purchased = []
+  		@pending_request = "no"
   	end
 
-
+  	def purchase_request(puppy, a_list)
+  		if (a_list.list).include?(puppy)
+  			@pending_request = "yes"
+  			# puppy.status = "pending"
+  		end
+  	end
   end
 end
