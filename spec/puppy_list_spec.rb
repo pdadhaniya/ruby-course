9e338b3a04c4e58fc7a77b977320ccdb3073f137
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-29 13:04:30 -0700

    add test to check if new puppy is added to list

--- spec/puppy_list_spec.rb
+++ spec/puppy_list_spec.rb
@@ -1,10 +1,19 @@
 require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::PuppyList do
+	let(:list) { PuppyBreeder::PuppyList.new}
+	let(:puppy) { PuppyBreeder::Puppy.new("Fred")}
+
 	describe "#initialize" do
 		it "should make a new puppy list" do
 			result = PuppyBreeder::PuppyList.new
 			expect(result.list).to eq([])
+		end
+	end
+
+	describe "#add" do
+		it "should add puppy to the list" do
+			expect((list.add(puppy)).length).to eq(1)
 		end
 	end
 end
