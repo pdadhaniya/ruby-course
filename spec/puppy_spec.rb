2d9d2e8fd820269600f99d5680d0e67aa4331c0b
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-29 13:32:13 -0700

    add test to check if sold method changes status to sold

--- spec/puppy_spec.rb
+++ spec/puppy_spec.rb
@@ -1,12 +1,18 @@
 require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::Puppy do
+	let(:puppy) { PuppyBreeder::Puppy.new("Fred")}
+
 	describe "#initialize" do
 		it "should make a new puppy" do
-			result = PuppyBreeder::Puppy.new("Fred")
-			expect(result.name).to eq("Fred")
+			expect(puppy.name).to eq("Fred")
 		end
+	end
 
+	describe "#sold" do
+		it "should sell a puppy" do
+			expect(puppy.sold).to eq("sold")
+		end
 	end
 
 end
