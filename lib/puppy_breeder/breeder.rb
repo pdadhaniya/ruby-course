432cd7d430fcd27dbc84abf9069a7b3669390f43
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-30 12:24:31 -0700

    update to include initialize method, add puppy method, add_purchase_request method, complete_purchase method

---
+++ lib/puppy_breeder/breeder.rb
@@ -0,0 +1,46 @@
+#Refer to this class as PuppyBreeder::Breeder
+module PuppyBreeder
+  class Breeder
+
+  	attr_accessor :breeder_request_list
+  	attr_reader :completed_purchase
+  	attr_reader :breeder_request_list
+
+  	#initialize breeder
+  	def initialize(breeder_name)
+  		@breeder_name = breeder_name
+  		@puppy_list = PuppyBreeder::PuppyList.new
+  		@breeder_request_list = PuppyBreeder::PurchaseRequestList.new
+  		@completed_purchase = {}
+  		@breeders_customers = PuppyBreeder::CustomerList.new 
+  	end
+
+  	def add_puppy(puppy)
+  		@puppy_list << puppy
+  	end
+  		#take from puppy list
+
+  	#input purchase request
+  	def add_purchase_request(puppy, customer)
+  		@breeder_request_list.purchase_request_list << (PuppyBreeder::PurchaseRequest.new(puppy, customer)).purchase_request
+    end
+
+  	#accept purchase request
+  	def complete_purchase(puppy, customer)
+  		@completed_purchase[puppy] = customer
+  		@breeder_request_list.purchase_request_list.delete({puppy => customer})
+  		@breeders_customers.customer_list[customer] = puppy
+  		@puppy_list.list.delete(puppy)
+  	end
+  		#and add to customer list
+  		#take from customer
+
+  	#view completed purchases
+  	# def view_purchases
+  	# 	@completed_purchase
+  	# end
+
+  	#
+  end
+
+end
