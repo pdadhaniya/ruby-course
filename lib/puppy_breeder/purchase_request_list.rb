be39a10a9e92c9432c81be8908b6afdc75008f88
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-30 12:27:33 -0700

    add class to initialize a purchase request list

---
+++ lib/puppy_breeder/purchase_request_list.rb
@@ -0,0 +1,13 @@
+#Refer to this class as PuppyBreeder::PurchaseRequestList
+module PuppyBreeder
+	class PurchaseRequestList
+
+		attr_accessor :purchase_request_list
+
+		def initialize
+			@purchase_request_list = []
+		end
+
+	end
+
+end
