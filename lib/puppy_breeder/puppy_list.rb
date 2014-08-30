f624e6c0c100be7d1992e784eecd815b3201ac43
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-30 12:26:11 -0700

    update to only include puppy_list method

--- lib/puppy_breeder/puppy_list.rb
+++ lib/puppy_breeder/puppy_list.rb
@@ -3,26 +3,26 @@ module PuppyBreeder
   class PuppyList
 
   	attr_accessor :list
-  	attr_reader :sold_puppies
 
   	def initialize
   		@list = []
-  		@sold_puppies = []
+  		# @sold_puppies = []
   	end
 
-  	def add(puppy)
-  		@list << puppy
-  	end
+    #moved to breeder
+  	# def add(puppy)
+  	# 	@list << puppy
+  	# end
 
-  	def puppy_sold(puppy)
-  		unless @list.include?(puppy)	
-  			false
-  		else
-  			puppy.status = "sold"
-  			@sold_puppies << puppy
-  			@list.delete(puppy)
-  			puppy
-  		end
-	end
+    #check if sold
+  	# def puppy_sold(puppy)
+  	# 	# unless @list.include?(puppy)	
+  	# 	# 	false
+  	# 	# else
+  	# 	# 	puppy.status = "sold"
+  	# 	# 	@sold_puppies << puppy
+  	# 	# 	@list.delete(puppy)
+  	# 	# 	puppy
+  	# 	end
   end
 end
