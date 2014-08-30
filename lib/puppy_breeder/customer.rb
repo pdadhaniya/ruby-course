4882e44ef255a9e737588df5053c45f2855c8de0
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-30 12:24:57 -0700

    update to only include initialize method and purchase_request method

--- lib/puppy_breeder/customer.rb
+++ lib/puppy_breeder/customer.rb
@@ -3,34 +3,15 @@ module PuppyBreeder
   class Customer
 
   	attr_reader :name
-  	attr_reader :puppy_purchased
-  	attr_reader :pending_request
+  	attr_reader :purchase_request
 
   	def initialize(name)
   		@name = name
-  		@puppy_purchased = []
-  		@pending_request = "no"
   	end
 
-  	def purchase_request(puppy, a_list)
-  		if (a_list.list).include?(puppy)
-  			@pending_request = "yes"
-  			puppy.status = "pending"
-  		else
-  			false
-  		end
+  	def purchase_request(breeder, puppy)
+        breeder.breeder_request_list.purchase_request_list << PuppyBreeder::PurchaseRequest.new(puppy, self)
   	end
-
-  	def complete_purchase(puppy, a_list)
-  		if @pending_request == "yes"
-  			@pending_request = "no"
-  			@puppy_purchased << puppy
-  			a_list.puppy_sold(puppy)
-  		else
-  			false
-  		end
-  	end
-
 
   end
 end
