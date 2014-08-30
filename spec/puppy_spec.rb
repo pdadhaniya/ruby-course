387a8b0b3b417db631da768f3eb093a4be97bfeb
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-30 12:29:40 -0700

    prepare file for tests based on new methods

--- spec/puppy_spec.rb
+++ spec/puppy_spec.rb
@@ -4,8 +4,12 @@ describe PuppyBreeder::Puppy do
 	let(:puppy) { PuppyBreeder::Puppy.new("Fred")}
 
 	describe "#initialize" do
-		it "should make a new puppy" do
+		xit "should make a new puppy" do
 			expect(puppy.name).to eq("Fred")
+		end
+
+		xit "should make a new puppy's status as available" do
+			expect(puppy.status).to eq("available")
 		end
 	end
 
