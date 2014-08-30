769c692fffb372febb93bef61eb536f31f155bd0
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-30 12:37:17 -0700

    update test to include test of initializing

--- spec/puppy_spec.rb
+++ spec/puppy_spec.rb
@@ -4,13 +4,10 @@ describe PuppyBreeder::Puppy do
 	let(:puppy) { PuppyBreeder::Puppy.new("Fred")}
 
 	describe "#initialize" do
-		xit "should make a new puppy" do
+		it "should make a new puppy" do
 			expect(puppy.name).to eq("Fred")
 		end
 
-		xit "should make a new puppy's status as available" do
-			expect(puppy.status).to eq("available")
-		end
 	end
 
 end
