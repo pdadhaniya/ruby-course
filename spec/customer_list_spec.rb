fc93ec7d5a507194cd7ffa2c81dda00fff559103
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-30 13:53:19 -0700

    add test to check if customer list is initialized with a hash

--- spec/customer_list_spec.rb
+++ spec/customer_list_spec.rb
@@ -1,23 +1,15 @@
-# require_relative 'spec_helper.rb'
+require_relative 'spec_helper.rb'
 
-# describe PuppyBreeder::CustomerList do
+describe PuppyBreeder::CustomerList do
+	let(:list_of_customers) { PuppyBreeder::CustomerList.new }
 
-
-
-
-
-
-
-
-
-
-
+	describe "#initialize" do
+		it "should make a new customer list hash" do
+			expect(list_of_customers.customer_list).to eq({})
+			list_of_customers.customer_list["a"] = "b"
+			expect(list_of_customers.customer_list).to eq({"a" => "b"})
+		end
+	end
 
 
-
-
-
-
-
-
-# end
+end
