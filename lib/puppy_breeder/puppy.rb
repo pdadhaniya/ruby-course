217e24d8b94dc1f47e1a68ff13895fb0548bb6e3
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-30 12:23:51 -0700

    update class to only have initialize method

--- lib/puppy_breeder/puppy.rb
+++ lib/puppy_breeder/puppy.rb
@@ -2,15 +2,14 @@
 module PuppyBreeder
   class Puppy
 	attr_reader :name
-	attr_accessor :status
 
   	def initialize(name)
   		@name = name
-  		@status = "available"
   	end
 
   end
 end
+
 
 
 
