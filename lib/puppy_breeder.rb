208f4d35934b4e02252837058367a19ae7b0593a
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-30 12:28:07 -0700

    update to include new class rb files

--- lib/puppy_breeder.rb
+++ lib/puppy_breeder.rb
@@ -5,3 +5,7 @@ end
 require_relative 'puppy_breeder/puppy.rb'
 require_relative 'puppy_breeder/purchase_request.rb'
 require_relative 'puppy_breeder/puppy_list.rb'
+require_relative 'puppy_breeder/breeder.rb'
+require_relative 'puppy_breeder/customer_list.rb'
+require_relative 'puppy_breeder/customer.rb'
+require_relative 'puppy_breeder/purchase_request_list.rb'
