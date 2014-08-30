cec32aae6ab0cb9d1a7a5b65aa4f4b84d3b44582
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-30 12:25:50 -0700

    update to only include initialize method

---
+++ lib/puppy_breeder/customer_list.rb
@@ -0,0 +1,19 @@
+#Refer to this class as PuppyBreeder::CustomerList
+module PuppyBreeder
+	
+	class CustomerList
+
+		attr_accessor :customer_list
+
+		def initialize
+			@customer_list = {}
+		end
+
+		# def add(customer)
+		# 	@customer_list << customer
+		# end
+
+	end
+
+end
+
