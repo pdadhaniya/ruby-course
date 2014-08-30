4f281d1ac48a19e5e1ca8f9b13c7cef9c496af54
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-30 12:27:10 -0700

    update to only include initialize method

--- lib/puppy_breeder/purchase_request.rb
+++ lib/puppy_breeder/purchase_request.rb
@@ -2,5 +2,22 @@
 module PuppyBreeder
   class PurchaseRequest
 
+  	attr_accessor :purchase_request
+
+  	def initialize(puppy, customer)
+      @purchase_request = {}
+  	end
+
+    #get from customer
+  	# def pending(puppy)
+  	# 	@pending_puppies << puppy
+  	# end
+
+    #get from breeder
+  	# def complete_request(puppy)
+  	# 	@completed_puppies << puppy
+  	# 	@pending_puppies.delete(puppy)
+  	# end
+
   end
 end
