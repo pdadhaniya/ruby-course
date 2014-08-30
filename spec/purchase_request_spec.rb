76802fdd615666de8d5e1521f4933036de4bd5c2
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-30 15:31:20 -0700

    add test to check if purchase request initializes with a hash with puppy key and customer value

--- spec/purchase_request_spec.rb
+++ spec/purchase_request_spec.rb
@@ -1,5 +1,17 @@
 require_relative 'spec_helper.rb'
 
-# describe PuppyBreeder::PurchaseRequest do
+describe PuppyBreeder::PurchaseRequest do
+	let(:puppy) { PuppyBreeder::Puppy.new("Fred")}
+	let(:buyer) { PuppyBreeder::Customer.new("David") }
 
-# end
+	describe "#initialize" do
+		it "should make a new purchase request hash with puppy and customer" do
+			result = PuppyBreeder::PurchaseRequest.new(puppy, buyer)
+			expect(result.purchase_request).to eq({ puppy => buyer })
+		end
+
+	end
+
+
+
+end
