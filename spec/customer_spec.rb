43efd1bfdfc98ae495188e5d3396bb99be4f7575
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-08-31 13:29:36 -0700

    add test to check if purchase_request method adds a purchase request to the purchase request list

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::Customer do
 	let(:puppy) { PuppyBreeder::Puppy.new("Fred")}
-	let(:new_list) { PuppyBreeder::PuppyList.new}
+	let(:maker) { PuppyBreeder::Breeder.new("Jim") }
 	let(:buyer) { PuppyBreeder::Customer.new("David") }
 	let(:buyer2) { PuppyBreeder::Customer.new("Paul")}
 
@@ -10,6 +10,14 @@ describe PuppyBreeder::Customer do
 		it "should add a name for each customer" do
 			expect(buyer.name).to eq("David")
 			expect(buyer2.name).to eq("Paul")
+		end
+	end
+
+	describe "#purchase_request" do
+		it "should add an instance of the purchase request class to the purchase request list" do
+			maker.add_puppy(puppy)
+			buyer.purchase_request(maker, puppy)
+			expect(maker.breeder_request_list.purchase_request_list).to eq([{puppy => buyer}])
 		end
 	end
 
