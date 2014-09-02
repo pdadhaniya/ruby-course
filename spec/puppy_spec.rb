a478bda9499193909812ce6d36806c68350d1ffc
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-09-02 10:18:00 -0700

    add breed attribute to new puppy let

--- spec/puppy_spec.rb
+++ spec/puppy_spec.rb
@@ -1,11 +1,12 @@
 require_relative 'spec_helper.rb'
 
 describe PuppyBreeder::Puppy do
-	let(:puppy) { PuppyBreeder::Puppy.new("Fred")}
+	let(:puppy) { PuppyBreeder::Puppy.new("Fred", "husky")}
 
 	describe "#initialize" do
 		it "should make a new puppy" do
 			expect(puppy.name).to eq("Fred")
+			expect(puppy.breed).to eq("husky")
 		end
 
 	end
