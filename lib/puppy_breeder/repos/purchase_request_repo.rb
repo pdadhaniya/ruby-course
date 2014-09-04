0a6bfd76c70d2211dd08919ede569dfb832c0d86
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-09-04 16:44:09 -0700

    update with methods from breeder that are more appropriate in this repo

--- lib/puppy_breeder/repos/purchase_request_repo.rb
+++ lib/puppy_breeder/repos/purchase_request_repo.rb
@@ -8,15 +8,33 @@ module PuppyBreeder
         @db = PG.connect(host: 'localhost', dbname: 'puppy-breeder-db')
       end
 
+      def add_purchase_request(puppy, customer)
+        if puppy.id.nil? #change status to unavailable
+          false
+        else 
+          sql = "INSERT INTO purchase_request( puppy, customer, status )
+            VALUES ( '#{puppy.id}', '#{customer.id}', 'pending' ) RETURNING *"
+          result = @db.exec(sql)
+        end
+      end
 
+      #def unavailable
+      #if puppies table is updated with a puppy with status unavailable, change to pending
 
+      def deny_request(puppy, customer)
+        sql = "UPDATE purchase_request SET ( status ) = ( 'denied' ) WHERE puppy='#{puppy.id}' AND customer='#{customer.id}' RETURNING *"
+        result = @db.exec(sql)
+      end
 
+      def complete_request(puppy, customer)
+        sql = "UPDATE purchase_request SET ( status ) = ( 'completed' ) WHERE puppy='#{puppy.id}' AND customer='#{customer.id}' RETURNING *"
+        result = @db.exec(sql)
+      end
 
-
-
-
-
-
+      def view_pending
+        sql = "SELECT * FROM purchase_request WHERE status='pending'"
+        result = @db.exec(sql)
+      end
 
       def drop_tables
         sql = "DROP TABLE purchase_request"
