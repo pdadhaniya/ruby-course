511958893eb054c99e8db2f24325ccfa142ec45b
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-09-04 16:43:48 -0700

    update to move methods relating to purchase request to purchase request repo

--- lib/puppy_breeder/repos/breeder_repo.rb
+++ lib/puppy_breeder/repos/breeder_repo.rb
@@ -8,48 +8,25 @@ module PuppyBreeder
 				@db = PG.connect(host: 'localhost', dbname: 'puppy-breeder-db')
 			end
 
-      def add_customer(customer)
-        if customer.id.nil?
-          sql = "INSERT INTO customer( name ) VALUES ( '#{customer.name}' ) returning *"
+      def add_breeder(breeder)
+        if breeder.id.nil?
+          sql = "INSERT INTO breeder( name ) VALUES ( '#{breeder.breeder_name}' ) returning *"
           result = @db.exec(sql)
-          customer.instance_variable_set("@id", (result[0]["id"]).to_i)
-          result.first
+          breeder.instance_variable_set("@id", (result[0]["id"]).to_i)
+          result
         end
       end
 
-      def add_purchase_request(puppy, customer)
-        if puppy.id.nil?
-          false
-        else 
-          sql = "INSERT INTO purchase_request( puppy, customer, status )
-            VALUES ( '#{puppy.id}', '#{customer.id}', 'pending' ) RETURNING *"
-          result = @db.exec(sql)
-        end
-      end
-
-
-      def drop_tables_breeder
+      def drop_tables
         sql = "DROP TABLE breeder"
         result = @db.exec(sql)
         result
       end
 
-      def drop_tables_customer
-        sql = "DROP TABLE customer"
-        result = @db.exec(sql)
-        result
-      end
-
-      def create_tables_breeder
+      def create_tables
         sql = "CREATE TABLE breeder( id SERIAL, name text, PRIMARY KEY ( id ) )"
         result = @db.exec(sql)
       end
-
-      def create_tables_customer
-        sql = "CREATE TABLE customer( id SERIAL, name text, PRIMARY KEY ( id ) )"
-        result = @db.exec(sql)
-      end
-
 
 		end
 	end
