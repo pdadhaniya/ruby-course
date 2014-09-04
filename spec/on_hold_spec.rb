a44c19250570f1dde5ff39a5965755a11b53b153
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-09-04 16:45:29 -0700

    prepare for removal of file

--- spec/on_hold_spec.rb
+++ spec/on_hold_spec.rb
@@ -1,16 +1,16 @@
-require_relative 'spec_helper.rb'
+# require_relative 'spec_helper.rb'
 
-describe PuppyBreeder::OnHold do 
-	let(:unavailable) { PuppyBreeder::OnHold.new }
+# describe PuppyBreeder::OnHold do 
+# 	let(:unavailable) { PuppyBreeder::OnHold.new }
 
-	describe "#initialize" do
-		it "should make an on hold list" do
-			expect(unavailable.on_hold_list).to eq([])
-			unavailable.on_hold_list << ({:dog => "puppy", :rank => 1})
-			expect(unavailable.on_hold_list).to eq([{:dog => "puppy", :rank => 1}])
-		end
-	end
+# 	describe "#initialize" do
+# 		it "should make an on hold list" do
+# 			expect(unavailable.on_hold_list).to eq([])
+# 			unavailable.on_hold_list << ({:dog => "puppy", :rank => 1})
+# 			expect(unavailable.on_hold_list).to eq([{:dog => "puppy", :rank => 1}])
+# 		end
+# 	end
 
 
 	
-end
+# end
