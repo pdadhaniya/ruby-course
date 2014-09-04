7534a99fe237934ec0524235d331034631717e45
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-09-04 13:06:20 -0700

    add status attribute

--- lib/puppy_breeder/purchase_request.rb
+++ lib/puppy_breeder/purchase_request.rb
@@ -3,9 +3,11 @@ module PuppyBreeder
   class PurchaseRequest
 
   	attr_accessor :purchase_request
+  	attr_accessor :status
 
   	def initialize(puppy, customer)
       @purchase_request = { puppy => customer }
+      @status = "pending"
   	end
 
   end
