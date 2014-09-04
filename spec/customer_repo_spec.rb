2dcbfa93392de5f3ed4243450c877aac1ce34de3
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-09-04 16:46:53 -0700

    update to reflect new references to other repos

---
+++ spec/customer_repo_spec.rb
@@ -0,0 +1,29 @@
+require_relative 'spec_helper.rb'
+
+describe PuppyBreeder::Repositories::CustomerRepo do 
+  let(:fred) { PuppyBreeder::Puppy.new("Fred", "collie")}
+  let(:bob) { PuppyBreeder::Puppy.new("Bob", "pitbull")}
+  let(:customer) { PuppyBreeder::Customer.new("Robert") }
+  let(:breeder) { PuppyBreeder::Breeder.new("Puppymaster")}
+
+  before(:each) do
+    PuppyBreeder.purchase_repo.drop_tables
+    PuppyBreeder.customer_repo.drop_tables
+    PuppyBreeder.breeder_repo.drop_tables
+    PuppyBreeder.puppy_repo.drop_tables
+    PuppyBreeder.puppy_repo.create_tables
+    PuppyBreeder.breeder_repo.create_tables
+    PuppyBreeder.customer_repo.create_tables
+    PuppyBreeder.purchase_repo.create_tables
+  end
+
+  describe "#add_customer" do
+    it "should update the customer table with name of the customer" do
+      result = PuppyBreeder.customer_repo.add_customer(customer)
+      expect(result["id"]).to eq("1")
+      expect(result["name"]).to eq("Robert")
+    end
+  end
+
+
+end
