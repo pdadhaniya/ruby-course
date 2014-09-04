63490ea2807563202f5c7d2a404e4f576b9fd228
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-09-04 13:05:58 -0700

    add id attribute to puppy

--- lib/puppy_breeder/puppy.rb
+++ lib/puppy_breeder/puppy.rb
@@ -3,10 +3,12 @@ module PuppyBreeder
   class Puppy
 	attr_reader :name
 	attr_reader :breed
+	attr_accessor :id
 
   	def initialize(name, breed)
   		@name = name
   		@breed = breed
+  		@id = nil
   	end
 
   end
