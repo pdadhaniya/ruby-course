83e7a69b258950efee7f05e25d4171926bdcf6de
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-09-07 00:38:16 -0700

    refactored to make methods work for arbitrary number of categories

--- lib/complex_predictor.rb
+++ lib/complex_predictor.rb
@@ -42,51 +42,58 @@ class ComplexPredictor < Predictor
         end
       end
     end
-    @top_fifty_astronomy = Hash.new(0)
-    @sorted_words_astronomy = (@category_word_count[:astronomy]).sort { |a, b| b[1]<=>a[1]}
-    a = 0
-    while a < 100
-      @top_fifty_astronomy[@sorted_words_astronomy[a][0]] = @sorted_words_astronomy[a][1]
-      a += 1
+
+    @top_words = Hash.new
+    @sorted_words = Hash.new
+    @category_word_count.each do |category, words_count|
+      @sorted_words[category] = (@category_word_count[category]).sort {|a, b| b[1]<=>a[1]}
     end
-    # @astronomy_array = []
-    # @top_fifty_astronomy.each do |word, count|
-    #   @astronomy_array << word
-    # end
-    @top_fifty_philosophy = Hash.new(0)
-    @sorted_words_philosophy = (@category_word_count[:philosophy]).sort { |a, b| b[1]<=>a[1]}
-    b = 0
-    while b < 100
-      @top_fifty_philosophy[@sorted_words_philosophy[b][0]] = @sorted_words_philosophy[b][1]
-      b += 1
+    
+    @sorted_words.each do |category, words_count|
+      @top_words[category] = {}
     end
-    # @philosophy_array = []
-    # @top_fifty_philosophy.each do |word, count|
-    #   @philosophy_array << word
-    # end
-    @top_fifty_archeology = Hash.new(0)
-    @sorted_words_archeology = (@category_word_count[:archeology]).sort { |a, b| b[1]<=>a[1]}
-    c = 0
-    while c < 100
-      @top_fifty_archeology[@sorted_words_archeology[c][0]] = @sorted_words_archeology[c][1]
-      c += 1
+
+    @top_words.each do |category, words_hash|
+      p = 0
+      while p < 10
+        @top_words[category][@sorted_words[category][p][0]] = @sorted_words[category][p][1]
+        p += 1
+      end
     end
-    # @archeology_array = []
-    # @top_fifty_archeology.each do |word, count|
-    #   @archeology_array << word
+
+    # @top_fifty_astronomy = Hash.new(0)
+    # @sorted_words_astronomy = (@category_word_count[:astronomy]).sort { |a, b| b[1]<=>a[1]}
+    # a = 0
+    # while a < 100
+    #   @top_fifty_astronomy[@sorted_words_astronomy[a][0]] = @sorted_words_astronomy[a][1]
+    #   a += 1
     # end
-    @top_fifty_religion = Hash.new(0)
-    @sorted_words_religion = (@category_word_count[:religion]).sort { |a, b| b[1]<=>a[1]}
-    d = 0
-    while d < 100
-      @top_fifty_religion[@sorted_words_religion[d][0]] = @sorted_words_religion[d][1]
-      d += 1
-    end
-    # @religion_array = []
-    # @top_fifty_religion.each do |word, count|
-    #   @religion_array << word
+
+    # @top_fifty_philosophy = Hash.new(0)
+    # @sorted_words_philosophy = (@category_word_count[:philosophy]).sort { |a, b| b[1]<=>a[1]}
+    # b = 0
+    # while b < 100
+    #   @top_fifty_philosophy[@sorted_words_philosophy[b][0]] = @sorted_words_philosophy[b][1]
+    #   b += 1
     # end
-    @top_fifty = {:astronomy => @top_fifty_astronomy, :philosophy => @top_fifty_philosophy, :archeology => @top_fifty_archeology, :religion => @top_fifty_religion}
+
+    # @top_fifty_archeology = Hash.new(0)
+    # @sorted_words_archeology = (@category_word_count[:archeology]).sort { |a, b| b[1]<=>a[1]}
+    # c = 0
+    # while c < 100
+    #   @top_fifty_archeology[@sorted_words_archeology[c][0]] = @sorted_words_archeology[c][1]
+    #   c += 1
+    # end
+
+    # @top_fifty_religion = Hash.new(0)
+    # @sorted_words_religion = (@category_word_count[:religion]).sort { |a, b| b[1]<=>a[1]}
+    # d = 0
+    # while d < 100
+    #   @top_fifty_religion[@sorted_words_religion[d][0]] = @sorted_words_religion[d][1]
+    #   d += 1
+    # end
+
+    # @top_fifty = {:astronomy => @top_fifty_astronomy, :philosophy => @top_fifty_philosophy, :archeology => @top_fifty_archeology, :religion => @top_fifty_religion}
 
   end
 
@@ -113,41 +120,41 @@ class ComplexPredictor < Predictor
     @test_top_fifty.each do |word, count|
       @test_array << word
     end
-    @astronomy_count = 0
-    @archeology_count = 0
-    @philosophy_count = 0
-    @religion_count = 0
-    @test_array.each do |x|
-      # @astronomy_count += 1 if @astronomy_array.include?(x)
-      @astronomy_count += 1 if (@top_fifty[:astronomy]).key?(x)
-      # @archeology_count += 1 if @archeology_array.include?(x)
-      @archeology_count += 1 if (@top_fifty[:archeology]).key?(x)
-      # @philosophy_count += 1 if @philosophy_array.include?(x)
-      @philosophy_count += 1 if (@top_fifty[:philosophy]).key?(x)
-      # @religion_count += 1 if @religion_array.include?(x)
-      @religion_count += 1 if (@top_fifty[:religion]).key?(x)
+    # @astronomy_count = 0
+    # @archeology_count = 0
+    # @philosophy_count = 0
+    # @religion_count = 0
+
+    @pop_word = Hash.new(0)
+    @top_words.each do |category, word_count|
+      @pop_word[category] = 0
     end
-    if @astronomy_count > @archeology_count && @astronomy_count > @philosophy_count && @astronomy_count > @religion_count
-      :astronomy
-    elsif @archeology_count > @philosophy_count && @archeology_count > @religion_count
-      :archeology
-    elsif @philosophy_count > @religion_count
-      :philosophy
-    else
-      :religion
+
+    @pop_word.each do |category, count|
+      q = 0
+      while q < 100
+        @pop_word[category] += 1 if (@top_words[category]).key?(@test_array[q])
+        q += 1
+      end
     end
-    # @top_twenty.each do |category, hash|
-    #   hash.each do |word, count|
-    #     counter = 0
-    #     while counter < tokens.length
-    #       if @sorted_word_tally[counter][0] == word
-    #         counter = tokens.length
-    #         return category
-    #       else
-    #         counter += 1
-    #       end
-    #     end
-    #   end
+
+    winner = @pop_word.max_by { |k, v| v }
+    return winner[0]
+    # @test_array.each do |x|
+
+    #   @astronomy_count += 1 if (@top_words[:astronomy]).key?(x)
+    #   @archeology_count += 1 if (@top_words[:archeology]).key?(x)
+    #   @philosophy_count += 1 if (@top_words[:philosophy]).key?(x)
+    #   @religion_count += 1 if (@top_words[:religion]).key?(x)
+    # end
+    # if @astronomy_count > @archeology_count && @astronomy_count > @philosophy_count && @astronomy_count > @religion_count
+    #   :astronomy
+    # elsif @archeology_count > @philosophy_count && @archeology_count > @religion_count
+    #   :archeology
+    # elsif @philosophy_count > @religion_count
+    #   :philosophy
+    # else
+    #   :religion
     # end
     # :astronomy
   end
