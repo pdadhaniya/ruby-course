d2b42ec2d883edc4659ae930fe8ec79f29f00145
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-09-10 13:14:24 -0700

    add get / to server.rb

--- web/server.rb
+++ web/server.rb
@@ -1,16 +1,23 @@
 require_relative '../lib/songify.rb'
 require 'sinatra/base'
+# require 'rubygems'
+
 
 class Songify::Server < Sinatra::Application
+set :bind, '0.0.0.0'
+
+  get '/' do
+    @songs = Songify.songs_repo.get_all_songs
+    erb :index
+  end
 
 
 
 
 
 
-
-
-
+run! if app_file == $0 #what does this mean?!?!
 
 
 end
+
