d48a015ab8588818fb0e1aa4716c16cc53572e0d
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-09-15 15:13:31 -0700

    update all song tests to incorporate the artist argument being an array of artists

--- spec/entities/song_spec.rb
+++ spec/entities/song_spec.rb
@@ -1,13 +1,20 @@
 require_relative '../spec_helper.rb'
 
 describe Songify::Song do 
-  let(:song1) { Songify::Song.new("Happy Birthday", "Rap", "Drake")}
+  let(:song1) { Songify::Song.new("Happy Birthday", "Rap", ["Drake"])}
 
   describe "#initialize" do
     it "should make a new song" do
       expect(song1.title).to eq("Happy Birthday")
       expect(song1.genre).to eq("Rap")
-      expect(song1.artist).to eq("Drake")
+      expect(song1.artist).to eq(["Drake"])
+    end
+
+    it "should make a new song with multiple artists if an array of artists is the argument" do
+      song2 = Songify::Song.new("Hey", "Classical", ["Drake", "Jay-Z"])
+      expect(song2.title).to eq("Hey")
+      expect(song2.genre).to eq("Classical")
+      expect(song2.artist).to eq(["Drake", "Jay-Z"])
     end
   end
 
--- spec/repositories/songs_spec.rb
+++ spec/repositories/songs_spec.rb
@@ -2,7 +2,7 @@ require_relative '../spec_helper.rb'
 
 describe Songify::Repositories::SongsRepo do 
   let(:genre1) { Songify::Genre.new("Rap")}
-  let(:song1) { Songify::Song.new("Happy Birthday", "Rap", "Drake")}
+  let(:song1) { Songify::Song.new("Happy Birthday", "Rap", ["Drake"])}
 
   before(:each) do
     Songify.songs_artists_repo.drop_tables
@@ -48,7 +48,7 @@ describe Songify::Repositories::SongsRepo do
     it "should return all songs in the songs table" do
       genre2 = Songify::Genre.new("Classical")
       Songify.genres_repo.save_genre(genre2)
-      song2 = Songify::Song.new("Hotel California", "Classical", "Eagles")
+      song2 = Songify::Song.new("Hotel California", "Classical", ["Eagles"])
       Songify.songs_repo.save_song(song1)
       Songify.songs_repo.save_song(song2)
       result = Songify.songs_repo.get_all_songs
